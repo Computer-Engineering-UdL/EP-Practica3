9f08a2855e3efd77a673b83cb0bf4cfc7f51d01f
Aniol0012 <aniol0012@gmail.com>
2024-12-25 13:17:22 +0100

    test: add tests for geographical point

--- src/utils/NumberUtils.java
+++ src/utils/NumberUtils.java
@@ -1,9 +1,18 @@
 package utils;
 
 import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
 
 public final class NumberUtils {
     public static String generateUUID() {
         return UUID.randomUUID().toString().replace("-", "");
+    }
+
+    public static float generateRandomLatitude() {
+        return (float) ThreadLocalRandom.current().nextDouble(-90, 90);
+    }
+
+    public static float generateRandomLongitude() {
+        return (float) ThreadLocalRandom.current().nextDouble(-180, 180);
     }
 }
---
+++ test/data/GeographicPointTest.java
@@ -0,0 +1,62 @@
+package data;
+
+import org.junit.jupiter.api.Test;
+import utils.NumberUtils;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GeographicPointTest {
+    @Test
+    void testGeographicPointCreation() {
+        float latitude = NumberUtils.generateRandomLatitude();
+        float longitude = NumberUtils.generateRandomLongitude();
+        GeographicPoint point = new GeographicPoint(latitude, longitude);
+        assertEquals(latitude, point.getLatitude());
+        assertEquals(longitude, point.getLongitude());
+    }
+
+    @Test
+    void testGeographicPointEquality() {
+        float latitude = NumberUtils.generateRandomLatitude();
+        float longitude = NumberUtils.generateRandomLongitude();
+        GeographicPoint point1 = new GeographicPoint(latitude, longitude);
+        GeographicPoint point2 = new GeographicPoint(latitude, longitude);
+        assertEquals(point1, point2);
+        assertEquals(point1.hashCode(), point2.hashCode());
+    }
+
+    @Test
+    void testGeographicPointInequality() {
+        float latitude1 = NumberUtils.generateRandomLatitude();
+        float longitude1 = NumberUtils.generateRandomLongitude();
+
+        float latitude2 = NumberUtils.generateRandomLatitude();
+        float longitude2 = NumberUtils.generateRandomLongitude();
+
+        GeographicPoint point1 = new GeographicPoint(latitude1, longitude1);
+        GeographicPoint point2 = new GeographicPoint(latitude2, longitude2);
+
+        // In case coordinates are the same, the test is called again
+        if (point1.equals(point2)) {
+            testGeographicPointInequality();
+            return;
+        }
+
+        assertNotEquals(point1, point2);
+        assertNotEquals(point1.hashCode(), point2.hashCode());
+    }
+
+    @Test
+    void testGeographicPointZeroCoordinates() {
+        GeographicPoint point = new GeographicPoint(0.0f, 0.0f);
+        assertEquals(0.0f, point.getLatitude());
+        assertEquals(0.0f, point.getLongitude());
+    }
+
+    @Test
+    void testGeographicPointToString() {
+        GeographicPoint point = new GeographicPoint(NumberUtils.generateRandomLatitude(), NumberUtils.generateRandomLongitude());
+        assertEquals("Geographic point: {latitude = " + point.getLatitude() +
+                " longitude = " + point.getLongitude() + "}", point.toString());
+    }
+}
